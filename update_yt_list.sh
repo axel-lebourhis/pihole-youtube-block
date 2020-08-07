f7594455d5b2855cd895129963ce28cd91e639ae
Axel Le Bourhis <lebourhis.axel@hotmail.fr>
2020-08-08 00:51:48 +0200

    keep old list
    Signed-off-by: Axel Le Bourhis <lebourhis.axel@hotmail.fr>

--- update_yt_list.sh
+++ update_yt_list.sh
@@ -2,10 +2,24 @@
 
 DIR=
 
+if [ -f "/var/www/html/youtube-ads-list.txt" ]
+then
+	EXIST_LIST=$(cat /var/www/html/youtube-ads-list.txt)
+fi
+
 # update dns list
-python ${DIR}/get_yt_dns.py | grep ^r | awk '{print $1}' | sort -nr | uniq > /var/www/html/youtube-ads-list.txt
-grep r*.googlevideo.com /var/log/pihole.log | awk '{print $6}'| grep ^r | sort -nr | uniq >> /var/www/html/youtube-ads-list.txt
-echo "gstaticadssl.l.google.com" >> /var/www/html/youtube-ads-list.txt
+NEW_LIST="$(python ${DIR}/get_yt_dns.py | grep ^r | awk '{print $1}' | sort -nr | uniq)"
+NEW_LIST="${NEW_LIST} $(grep r*.googlevideo.com /var/log/pihole.log | awk '{print $6}'| grep ^r | sort -nr | uniq)"
+NEW_LIST="${NEW_LIST} $(echo "gstaticadssl.l.google.com")"
+
+FINAL_LIST="$(echo ${EXIST_LIST} ${NEW_LIST} | sort -nr | uniq)"
+
+echo "" > /var/www/html/youtube-ads-list.txt
+
+for ADDR in ${FINAL_LIST} 
+do
+	echo "${ADDR}" >> /var/www/html/youtube-ads-list.txt
+done
 
 # update gravity
 pihole -g
