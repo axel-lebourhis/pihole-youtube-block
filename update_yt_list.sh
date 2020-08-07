8bb0edf2663abfa5698f222be8ea06136ed9bf6f
Axel Le Bourhis <lebourhis.axel@hotmail.fr>
2020-08-08 01:06:15 +0200

    fix list keeping
    Signed-off-by: Axel Le Bourhis <lebourhis.axel@hotmail.fr>

--- update_yt_list.sh
+++ update_yt_list.sh
@@ -2,24 +2,23 @@
 
 DIR=
 
+echo "" > ./tmp_list.txt
+
 if [ -f "/var/www/html/youtube-ads-list.txt" ]
 then
-	EXIST_LIST=$(cat /var/www/html/youtube-ads-list.txt)
+	cat /var/www/html/youtube-ads-list.txt >> ./tmp_list.txt
 fi
 
 # update dns list
-NEW_LIST="$(python ${DIR}/get_yt_dns.py | grep ^r | awk '{print $1}' | sort -nr | uniq)"
-NEW_LIST="${NEW_LIST} $(grep r*.googlevideo.com /var/log/pihole.log | awk '{print $6}'| grep ^r | sort -nr | uniq)"
-NEW_LIST="${NEW_LIST} $(echo "gstaticadssl.l.google.com")"
+python ${DIR}/get_yt_dns.py | grep ^r | awk '{print $1}' | sort -nr | uniq >> ./tmp_list.txt
+grep r*.googlevideo.com /var/log/pihole.log | awk '{print $6}'| grep ^r | sort -nr | uniq >> ./tmp_list.txt
+echo "gstaticadssl.l.google.com" >> ./tmp_list.txt
 
-FINAL_LIST="$(echo ${EXIST_LIST} ${NEW_LIST} | sort -nr | uniq)"
-
+# write new list
 echo "" > /var/www/html/youtube-ads-list.txt
+echo ./tmp_list.txt | sort -nr | uniq >> /var/www/html/youtube-ads-list.txt
 
-for ADDR in ${FINAL_LIST} 
-do
-	echo "${ADDR}" >> /var/www/html/youtube-ads-list.txt
-done
+rm ./tmp_list.txt
 
 # update gravity
 pihole -g
